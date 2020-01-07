07ecc501d1bc0659add835ab333078b5e6eb53ac
Valeriy Makarshin <valerymakarshin@gmail.com>
2020-01-07 16:12:17 +0300

    [LocalStorage] Рефакторинг аннотаций.

--- debug-panel/src/main/kotlin/com/redmadrobot/debug_panel/accounts/data/model/DebugUserCredentials.kt
+++ debug-panel/src/main/kotlin/com/redmadrobot/debug_panel/accounts/data/model/DebugUserCredentials.kt
@@ -6,8 +6,12 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "debug_user_credentials")
 data class DebugUserCredentials(
-    @PrimaryKey @ColumnInfo(name = "login") val login: String,
-    @ColumnInfo(name = "password") val password: String
+    @PrimaryKey
+    @ColumnInfo(name = "login")
+    val login: String,
+
+    @ColumnInfo(name = "password")
+    val password: String
 ) {
     companion object {
         fun empty() = DebugUserCredentials("", "")
