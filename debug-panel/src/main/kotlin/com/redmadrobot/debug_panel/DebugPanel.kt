bef3441ad1aab44a2e89f816d85e25719e3893fb
Roman Choryev <r.choryev@redmadrobot.com>
2020-01-15 16:45:42 +0300

    [Common]Обработка ЖЦ Activity для debug-panel вынесена в отдельный класс ActivityLifecycleHandler

--- debug-panel/src/main/kotlin/com/redmadrobot/debug_panel/DebugPanel.kt
+++ debug-panel/src/main/kotlin/com/redmadrobot/debug_panel/DebugPanel.kt
@@ -1,79 +1,22 @@
 package com.redmadrobot.debug_panel
 
-import android.app.*
 import android.content.Context
-import android.content.Intent
-import android.os.Build
-import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
-import androidx.core.app.TaskStackBuilder
 import com.redmadrobot.debug_panel.accounts.Authenticator
-import com.redmadrobot.debug_panel.util.ActivityLifecycleCallbacksAdapter
+import com.redmadrobot.debug_panel.util.ActivityLifecycleHandler
 
 object DebugPanel {
 
-    private const val NOTIFICATION_CHANNEL_ID = "DEBUG_NOTIFICATION_CHANNEL"
-    private const val NOTIFICATION_ID = 1
-
     internal var authenticator: Authenticator? = null
 
-    //Счетчик открытых activity
-    private var openActivityCount = 0
-    private var notificationManager: NotificationManagerCompat? = null
 
     fun start(context: Context): DebugPanel {
-        registerActivityLifecycleCallback(context)
+        ActivityLifecycleHandler(context).register()
         return this
     }
 
     fun setAuthenticator(authenticator: Authenticator): DebugPanel {
         this.authenticator = authenticator
         return this
-    }
-
-    private fun registerActivityLifecycleCallback(context: Context) {
-        (context as? Application)?.registerActivityLifecycleCallbacks(
-            object : ActivityLifecycleCallbacksAdapter() {
-                override fun onActivityResumed(activity: Activity) {
-                    super.onActivityResumed(activity)
-                    if (openActivityCount == 0) showDebugNotification(context)
-                    ++openActivityCount
-                }
-
-                override fun onActivityPaused(activity: Activity) {
-                    super.onActivityPaused(activity)
-                    --openActivityCount
-                    if (openActivityCount == 0) {
-                        notificationManager?.cancel(NOTIFICATION_ID)
-                    }
-                }
-            }
-        )
-    }
-
-    private fun showDebugNotification(context: Context) {
-        notificationManager = NotificationManagerCompat.from(context)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val name = context.getString(R.string.debug_panel)
-            val importance = NotificationManager.IMPORTANCE_DEFAULT
-            val channel = NotificationChannel(NOTIFICATION_CHANNEL_ID, name, importance)
-            notificationManager?.createNotificationChannel(channel)
-        }
-
-        val debugActivityIntent = Intent(context, DebugActivity::class.java)
-        val debugActivityPendingIntent = TaskStackBuilder.create(context)
-            .addNextIntentWithParentStack(debugActivityIntent)
-            .getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT)
-
-        val notification = NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID)
-            .setSmallIcon(R.drawable.ic_debug_notification)
-            .setContentTitle(context.getString(R.string.notification_title))
-            .setOngoing(true)
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-            .setContentIntent(debugActivityPendingIntent)
-            .build()
-
-        notificationManager?.notify(NOTIFICATION_ID, notification)
     }
 
 }
---
+++ debug-panel/src/main/kotlin/com/redmadrobot/debug_panel/util/ActivityLifecycleHandler.kt
@@ -0,0 +1,76 @@
+package com.redmadrobot.debug_panel.util
+
+import android.app.*
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.app.TaskStackBuilder
+import com.redmadrobot.debug_panel.DebugActivity
+import com.redmadrobot.debug_panel.R
+
+class ActivityLifecycleHandler(private val context: Context) {
+
+    companion object {
+        private const val NOTIFICATION_CHANNEL_ID = "DEBUG_NOTIFICATION_CHANNEL"
+        private const val NOTIFICATION_ID = 1
+    }
+
+    //Счетчик открытых activity
+    private var openActivityCount = 0
+    private var notificationManager: NotificationManagerCompat? = null
+
+    fun register() {
+        (context as? Application)?.registerActivityLifecycleCallbacks(
+            object : ActivityLifecycleCallbacksAdapter() {
+                override fun onActivityResumed(activity: Activity) {
+                    super.onActivityResumed(activity)
+                    if (openActivityCount == 0) onResumed()
+                    ++openActivityCount
+                }
+
+                override fun onActivityPaused(activity: Activity) {
+                    super.onActivityPaused(activity)
+                    --openActivityCount
+                    if (openActivityCount == 0) onPaused()
+                }
+            }
+        )
+    }
+
+    private fun onPaused() {
+        notificationManager?.cancel(NOTIFICATION_ID)
+    }
+
+    private fun onResumed() {
+        showDebugNotification(context)
+    }
+
+
+    private fun showDebugNotification(context: Context) {
+        notificationManager = NotificationManagerCompat.from(context)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = context.getString(R.string.debug_panel)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(NOTIFICATION_CHANNEL_ID, name, importance)
+            notificationManager?.createNotificationChannel(channel)
+        }
+
+        val debugActivityIntent = Intent(context, DebugActivity::class.java)
+        val debugActivityPendingIntent = TaskStackBuilder.create(context)
+            .addNextIntentWithParentStack(debugActivityIntent)
+            .getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        val notification = NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_debug_notification)
+            .setContentTitle(context.getString(R.string.notification_title))
+            .setOngoing(true)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(debugActivityPendingIntent)
+            .build()
+
+        notificationManager?.notify(NOTIFICATION_ID, notification)
+    }
+
+}
