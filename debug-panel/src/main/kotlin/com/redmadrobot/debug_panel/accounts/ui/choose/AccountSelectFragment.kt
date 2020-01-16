6efd4ae741ed34430ae8853ba473617ac54195ef
Roman Choryev <r.choryev@redmadrobot.com>
2020-01-16 19:22:04 +0300

    [Account]Диалог для выбора аккаунтов переделан под обычный Fragment

--- debug-panel/src/main/kotlin/com/redmadrobot/debug_panel/accounts/ui/choose/AccountSelectFragment.kt
+++ debug-panel/src/main/kotlin/com/redmadrobot/debug_panel/accounts/ui/choose/AccountSelectFragment.kt
@@ -1,14 +1,10 @@
 package com.redmadrobot.debug_panel.accounts.ui.choose
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.redmadrobot.debug_panel.R
 import com.redmadrobot.debug_panel.accounts.data.accounts.AccountsProvider
 import com.redmadrobot.debug_panel.accounts.data.accounts.strategy.AccountRepositoryProvider
@@ -21,43 +17,14 @@ import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.kotlinandroidextensions.GroupieViewHolder
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.rxkotlin.subscribeBy
-import kotlinx.android.synthetic.main.bottom_sheet_account_select.*
+import kotlinx.android.synthetic.main.fragment_account_select.*
 
-class AccountSelectBottomSheet : BottomSheetDialogFragment() {
-
-    companion object {
-        private const val TAG = "AccountSelectBottomSheet"
-        const val REQUEST_CODE_ACCOUNT = 123123
-
-        fun <T> show(
-            fragmentManager: FragmentManager,
-            resultListener: T
-        ) where T : Fragment, T : AccountDataResultListener {
-
-            AccountSelectBottomSheet()
-                .apply {
-                    setTargetFragment(resultListener, REQUEST_CODE_ACCOUNT)
-                }
-                .show(fragmentManager, TAG)
-        }
-
-        fun show(fragmentManager: FragmentManager) {
-            AccountSelectBottomSheet()
-                .show(fragmentManager, TAG)
-        }
-    }
+class AccountSelectFragment : Fragment(R.layout.fragment_account_select) {
 
     private val compositeDisposable by lazy { CompositeDisposable() }
     private lateinit var accountRepositoryProvider: AccountRepositoryProvider
 
     private val accountsAdapter = GroupAdapter<GroupieViewHolder>()
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.bottom_sheet_account_select, container, false)
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -94,7 +61,6 @@ class AccountSelectBottomSheet : BottomSheetDialogFragment() {
             userCredentials.login,
             userCredentials.password
         )
-        dismiss()
     }
 
     private fun loadMockData() {
