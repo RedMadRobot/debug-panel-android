f2a7394037051e3e1021ede6742ffd393cb1bf46
Roman Choryev <r.choryev@redmadrobot.com>
2020-01-23 13:48:50 +0300

    [Accounts]В AccountsViewModel добавлены методы добавления и удаления аккаунтов

--- debug-panel/src/main/kotlin/com/redmadrobot/debug_panel/ui/accounts/AccountsViewModel.kt
+++ debug-panel/src/main/kotlin/com/redmadrobot/debug_panel/ui/accounts/AccountsViewModel.kt
@@ -3,6 +3,7 @@ package com.redmadrobot.debug_panel.ui.accounts
 import androidx.lifecycle.MutableLiveData
 import com.redmadrobot.debug_panel.data.accounts.AccountRepository
 import com.redmadrobot.debug_panel.data.accounts.AccountsProvider
+import com.redmadrobot.debug_panel.data.accounts.model.DebugUserCredentials
 import com.redmadrobot.debug_panel.extension.observeOnMain
 import com.redmadrobot.debug_panel.extension.zipList
 import com.redmadrobot.debug_panel.ui.accounts.item.UserCredentialsItem
@@ -24,6 +25,39 @@ class AccountsViewModel(
             .observeOnMain()
             .map { it.map(::UserCredentialsItem) }
             .subscribeBy(onSuccess = { accounts.value = it })
+            .autoDispose()
+    }
+
+    fun addAccount(account: DebugUserCredentials) {
+        accountsRepository
+            .addCredential(account)
+            .subscribeBy(
+                onComplete = {
+                    val newAccountList = (accounts.value as MutableList<Item>).apply {
+                        add(UserCredentialsItem(account))
+                    }
+                    accounts.value = newAccountList
+                }
+            )
+            .autoDispose()
+    }
+
+    fun removeAccount(position: Int) {
+        val accountItems = accounts.value as List<Item>
+        val item = accountItems[position] as UserCredentialsItem
+        val data = item.userCredentials
+
+        accountsRepository
+            .removeCredential(data)
+            .observeOnMain()
+            .subscribeBy(
+                onComplete = {
+                    val newAccountList = accountItems.toMutableList().apply {
+                        removeAt(position)
+                    }
+                    accounts.value = newAccountList
+                }
+            )
             .autoDispose()
     }
 }
