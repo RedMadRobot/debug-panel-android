847264dbb98bb14d6d846e5e0e6ae880205c479c
Roman Choryev <r.choryev@redmadrobot.com>
2020-02-16 16:20:38 +0300

    [AddServer]Добавлена разметка и навигация на экран добавления серверов

---
+++ debug-panel/src/main/kotlin/com/redmadrobot/debug_panel/ui/servers/add/AddServerFragment.kt
@@ -0,0 +1,12 @@
+package com.redmadrobot.debug_panel.ui.servers.add
+
+import com.redmadrobot.debug_panel.R
+import com.redmadrobot.debug_panel.ui.base.BaseFragment
+
+class AddServerFragment : BaseFragment(R.layout.fragment_add_server) {
+
+    companion object {
+        fun getInstance() = AddServerFragment()
+    }
+
+}
