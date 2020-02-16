b2bfbbdfbf0df763faa5b869c22a4865e9f99103
Roman Choryev <r.choryev@redmadrobot.com>
2020-02-16 17:09:52 +0300

    [Strategy]Интерфейс стратегии для работы с аккаунтами вынесен как общий интерфейс стратегии загрузки данных

--- debug-panel/src/main/kotlin/com/redmadrobot/debug_panel/data/accounts/AccountsProvider.kt
+++ debug-panel/src/main/kotlin/com/redmadrobot/debug_panel/data/accounts/AccountsProvider.kt
@@ -1,12 +1,12 @@
 package com.redmadrobot.debug_panel.data.accounts
 
+import com.redmadrobot.debug_panel.data.DebugDataLoadingStrategy
 import com.redmadrobot.debug_panel.data.accounts.model.DebugUserCredentials
-import com.redmadrobot.debug_panel.data.accounts.strategy.AccountsLoadStrategy
 import io.reactivex.Single
 
-class AccountsProvider(private val accountsLoadStrategy: AccountsLoadStrategy) {
+class AccountsProvider(private val accountsLoadStrategy: DebugDataLoadingStrategy<DebugUserCredentials>) {
 
     fun getAccounts(): Single<List<DebugUserCredentials>> {
-        return accountsLoadStrategy.loadAccounts()
+        return accountsLoadStrategy.loadData()
     }
 }
--- debug-panel/src/main/kotlin/com/redmadrobot/debug_panel/data/accounts/strategy/LocalAccountsLoadStrategy.kt
+++ debug-panel/src/main/kotlin/com/redmadrobot/debug_panel/data/accounts/strategy/LocalAccountsLoadStrategy.kt
@@ -1,13 +1,15 @@
 package com.redmadrobot.debug_panel.data.accounts.strategy
 
+import com.redmadrobot.debug_panel.data.DebugDataLoadingStrategy
 import com.redmadrobot.debug_panel.data.accounts.AccountRepository
 import com.redmadrobot.debug_panel.data.accounts.model.DebugUserCredentials
 import io.reactivex.Single
 
-class LocalAccountsLoadStrategy(private val accountRepository: AccountRepository) :
-    AccountsLoadStrategy {
+class LocalAccountsLoadStrategy(
+    private val accountRepository: AccountRepository
+) : DebugDataLoadingStrategy<DebugUserCredentials> {
 
-    override fun loadAccounts(): Single<List<DebugUserCredentials>> {
+    override fun loadData(): Single<List<DebugUserCredentials>> {
         return accountRepository.getCredentials()
     }
 }
