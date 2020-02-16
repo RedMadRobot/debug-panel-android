844f676ba0a18197b7dfba201d34cf32d170fcdf
Roman Choryev <r.choryev@redmadrobot.com>
2020-02-16 19:21:47 +0300

    [DebugServer]Запросы к БД вынесены на background поток

--- debug-panel/src/main/kotlin/com/redmadrobot/debug_panel/data/servers/LocalDebugServerRepository.kt
+++ debug-panel/src/main/kotlin/com/redmadrobot/debug_panel/data/servers/LocalDebugServerRepository.kt
@@ -4,20 +4,24 @@ import com.redmadrobot.debug_panel.data.storage.dao.DebugServersDao
 import com.redmadrobot.debug_panel.data.storage.entity.DebugServer
 import io.reactivex.Completable
 import io.reactivex.Single
+import io.reactivex.schedulers.Schedulers
 
-class LocalDebuServerRepository(
+class LocalDebugServerRepository(
     private val debugServersDao: DebugServersDao
 ) : DebugServerRepository {
 
     override fun addServer(server: DebugServer): Completable {
         return debugServersDao.insert(server)
+            .subscribeOn(Schedulers.io())
     }
 
     override fun getAllServers(): Single<List<DebugServer>> {
         return debugServersDao.getAll()
+            .subscribeOn(Schedulers.io())
     }
 
     override fun removeServer(server: DebugServer): Completable {
         return debugServersDao.remove(server)
+            .subscribeOn(Schedulers.io())
     }
 }
