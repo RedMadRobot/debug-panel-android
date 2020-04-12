7758c188540febb85d1649769e1b1f6c94e5e995
Roman Choryev <r.choryev@redmadrobot.com>
2020-04-12 19:14:38 +0300

    [AppSettings]SharedPreferences добавлен в DebugPanelConfig

--- debug-panel/src/main/kotlin/com/redmadrobot/debug_panel/internal/DebugPanelConfig.kt
+++ debug-panel/src/main/kotlin/com/redmadrobot/debug_panel/internal/DebugPanelConfig.kt
@@ -1,8 +1,10 @@
 package com.redmadrobot.debug_panel.internal
 
+import android.content.SharedPreferences
 import com.redmadrobot.debug_panel.accounts.Authenticator
 import com.redmadrobot.debug_panel.accounts.DefaultAuthenticator
 import com.redmadrobot.debug_panel.data.PreInstalledData
+import com.redmadrobot.debug_panel.data.settings.DefaultSharedPreferences
 import com.redmadrobot.debug_panel.data.storage.entity.DebugServer
 import com.redmadrobot.debug_panel.data.storage.entity.DebugUserCredentials
 import com.redmadrobot.debug_panel.inapp.toggles.FeatureTogglesConfig
@@ -11,5 +13,6 @@ data class DebugPanelConfig(
     val authenticator: Authenticator = DefaultAuthenticator(),
     val preInstalledServers: PreInstalledData<DebugServer> = PreInstalledData(emptyList()),
     val preInstalledAccounts: PreInstalledData<DebugUserCredentials> = PreInstalledData(emptyList()),
-    val featureTogglesConfig: FeatureTogglesConfig? = null
+    val featureTogglesConfig: FeatureTogglesConfig? = null,
+    val sharedPreferences: SharedPreferences = DefaultSharedPreferences()
 )
