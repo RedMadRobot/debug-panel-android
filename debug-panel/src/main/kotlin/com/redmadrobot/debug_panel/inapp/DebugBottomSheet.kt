aa8eab757f20ff9b48827e4dcad8f55031686c4e
Roman Choryev <r.choryev@redmadrobot.com>
2020-04-25 19:16:39 +0300

    [DebugPanel]Исправлена работа всплывающей панели.

--- debug-panel/src/main/kotlin/com/redmadrobot/debug_panel/inapp/DebugBottomSheet.kt
+++ debug-panel/src/main/kotlin/com/redmadrobot/debug_panel/inapp/DebugBottomSheet.kt
@@ -1,17 +1,20 @@
 package com.redmadrobot.debug_panel.inapp
 
+import android.app.Dialog
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
+import androidx.annotation.NonNull
 import androidx.fragment.app.FragmentManager
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.google.android.material.tabs.TabLayoutMediator
 import com.redmadrobot.debug_panel.R
-import kotlinx.android.synthetic.main.bottom_sheet_debug_panel.*
+import kotlinx.android.synthetic.main.bottom_sheet_debug_panel.view.*
 
-class DebugBottomSheet : BottomSheetDialogFragment() {
+open class DebugBottomSheet : BottomSheetDialogFragment() {
 
     companion object {
         private const val TAG = "DebugBottomSheet"
@@ -23,34 +26,48 @@ class DebugBottomSheet : BottomSheetDialogFragment() {
         }
     }
 
+    private var dialogView: View? = null
     private val tabLabelIds = arrayOf(R.string.accounts, R.string.servers, R.string.settings)
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.bottom_sheet_debug_panel, container)
+    @NonNull
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val dialog = super.onCreateDialog(savedInstanceState)
+        dialogView = dialog.layoutInflater.inflate(R.layout.bottom_sheet_debug_panel, null)
+        dialog.setOnShowListener {
+            setBottomSheetSize()
+        }
+        dialog.setContentView(requireNotNull(dialogView))
+        setViews(requireNotNull(dialogView))
+        return dialog
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        setViews()
+    private fun setBottomSheetSize() {
+        val dialogContainer = dialogView?.parent as? FrameLayout
+        dialogContainer?.apply {
+            layoutParams?.height = ViewGroup.LayoutParams.MATCH_PARENT
+            requestLayout()
+        }
+
+        with(getBehavior()) {
+            peekHeight = resources.displayMetrics.heightPixels / 2
+            state = BottomSheetBehavior.STATE_HALF_EXPANDED
+        }
     }
 
-    private fun setViews() {
-        debug_sheet_viewpager.adapter = DebugSheetViewPagerAdapter(requireActivity())
+    private fun setViews(dialogView: View) {
+        dialogView.debug_sheet_viewpager.adapter = DebugSheetViewPagerAdapter(requireActivity())
         val tabConfigurationStrategy = TabLayoutMediator.TabConfigurationStrategy { tab, position ->
             tab.setText(tabLabelIds[position])
         }
 
         TabLayoutMediator(
-            debug_sheet_tab_layout,
-            debug_sheet_viewpager,
+            dialogView.debug_sheet_tab_layout,
+            dialogView.debug_sheet_viewpager,
             tabConfigurationStrategy
         ).attach()
+    }
 
-        val sheetBehavior = (dialog as? BottomSheetDialog)?.behavior
-        sheetBehavior?.peekHeight = resources.displayMetrics.heightPixels
+    private fun getBehavior(): BottomSheetBehavior<FrameLayout> {
+        return (dialog as BottomSheetDialog).behavior
     }
 }
