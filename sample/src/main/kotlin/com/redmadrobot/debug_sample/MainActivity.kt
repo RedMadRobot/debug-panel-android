6cdd1dea124163b010fc28537c9e385c57729d1a
Roman Choryev <r.choryev@redmadrobot.com>
2020-04-25 17:37:47 +0300

    [Sample]Оновлен сэмпл. Добавлена возможность протестировать изменение сервера.

--- sample/src/main/kotlin/com/redmadrobot/debug_sample/MainActivity.kt
+++ sample/src/main/kotlin/com/redmadrobot/debug_sample/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.redmadrobot.debug_sample
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.redmadrobot.debug_panel.inapp.DebugBottomSheet
+import com.redmadrobot.debug_sample.network.ApiFactory
 import com.redmadrobot.debugpanel.R
 import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,6 +24,43 @@ class MainActivity : AppCompatActivity() {
         choose_account.setOnClickListener {
             chooseAccount()
         }
+        request_test.setOnClickListener {
+            makeTestRequest()
+        }
+    }
+
+    /**
+     * Создание тестового запроса для тестирования выбора сервера
+     * */
+    private fun makeTestRequest() {
+        ApiFactory.getSampleApi {
+            runOnUiThread {
+                showTestRequestToast(it)
+            }
+        }
+            .getTestData().enqueue(object : Callback<ResponseBody> {
+                override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                    /*do nothing*/
+                }
+
+                override fun onResponse(
+                    call: Call<ResponseBody>,
+                    response: Response<ResponseBody>
+                ) {
+                    /*do nothing*/
+                }
+            })
+    }
+
+    /**
+     * Отображаем адрес тестового запроса
+     * */
+    private fun showTestRequestToast(requestEndPoint: String) {
+        Toast.makeText(
+            this,
+            "Request to: $requestEndPoint",
+            Toast.LENGTH_LONG
+        ).show()
     }
 
     private fun chooseAccount() {
