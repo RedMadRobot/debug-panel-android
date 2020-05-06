153c0cef9e29bcdcdec9f06295d193fe3a5fd98e
Roman Choryev <r.choryev@redmadrobot.com>
2020-05-06 22:49:53 +0300

    [AppSettings]ApplicationSettingsFragment соединена с ApplicationSettingsViewModel

--- debug-panel/src/main/kotlin/com/redmadrobot/debug_panel/ui/settings/ApplicationSettingsFragment.kt
+++ debug-panel/src/main/kotlin/com/redmadrobot/debug_panel/ui/settings/ApplicationSettingsFragment.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.redmadrobot.debug_panel.R
+import com.redmadrobot.debug_panel.extension.observe
+import com.redmadrobot.debug_panel.extension.obtainViewModel
+import com.redmadrobot.debug_panel.internal.DebugPanel
 import com.redmadrobot.debug_panel.ui.base.BaseFragment
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.GroupieViewHolder
@@ -12,12 +15,19 @@ import kotlinx.android.synthetic.main.fragment_app_settings.*
 
 class ApplicationSettingsFragment : BaseFragment(R.layout.fragment_app_settings) {
 
-    companion object {
-        fun getInstance() = ApplicationSettingsFragment()
+    private val settingsViewModel by lazy {
+        obtainViewModel {
+            DebugPanel.getContainer().createApplicationSettingsViewModel()
+        }
     }
 
     private val settingsAdapter = GroupAdapter<GroupieViewHolder>()
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        observe(settingsViewModel.settings, ::setSettingList)
+        settingsViewModel.loadSettings()
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
