366637689a12753d012d3234b36b686eb191e6fa
Roman Choryev <r.choryev@redmadrobot.com>
2020-05-07 15:22:20 +0300

    [AppSettings]Добавлен в AppSettingsRepository метод обновления значения записи

--- debug-panel/src/main/kotlin/com/redmadrobot/debug_panel/data/settings/AppSettingsRepository.kt
+++ debug-panel/src/main/kotlin/com/redmadrobot/debug_panel/data/settings/AppSettingsRepository.kt
@@ -1,8 +1,11 @@
 package com.redmadrobot.debug_panel.data.settings
 
 import android.content.SharedPreferences
+import io.reactivex.Completable
 import io.reactivex.Single
 
 interface AppSettingsRepository {
     fun getSettings(): Single<List<SharedPreferences>>
+
+    fun updateSetting(key: String, value: Any): Completable
 }
