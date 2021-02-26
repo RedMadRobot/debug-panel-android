755cddf874e2df4f4c7b787372ce9f67a57b05c1
Roman Choryev <r.choryev@redmadrobot.com>
2021-02-26 11:57:13 +0300

    [Toggles]Модуль feature_toggles переведен на корутины

--- plugins/feature-toggles-plugin/src/main/kotlin/com/redmadrobot/feature_togles_plugin/ui/FeatureTogglesViewModel.kt
+++ plugins/feature-toggles-plugin/src/main/kotlin/com/redmadrobot/feature_togles_plugin/ui/FeatureTogglesViewModel.kt
@@ -1,13 +1,13 @@
 package com.redmadrobot.feature_togles_plugin.ui
 
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import com.redmadrobot.debug_panel_common.base.PluginViewModel
-import com.redmadrobot.debug_panel_core.extension.observeOnMain
+import com.redmadrobot.debug_panel_common.extension.safeLaunch
 import com.redmadrobot.feature_togles_plugin.data.FeatureToggleRepository
 import com.redmadrobot.feature_togles_plugin.data.model.FeatureToggle
 import com.redmadrobot.feature_togles_plugin.data.repository.PluginSettingsRepository
 import com.redmadrobot.feature_togles_plugin.ui.item.FeatureToggleItem
-import timber.log.Timber
 
 class FeatureTogglesViewModel(
     private val featureToggleRepository: FeatureToggleRepository,
@@ -17,42 +17,33 @@ class FeatureTogglesViewModel(
     val screenState = MutableLiveData<FeatureTogglesState>()
 
     fun loadFeatureToggles() {
-        val overrideEnable = pluginSettingsRepository.overrideFeatureToggleEnable
-        featureToggleRepository.getAllFeatureToggles()
-            .observeOnMain()
-            .map { items ->
-                items.map { FeatureToggleItem(it, ::updateFeatureToggleValue, overrideEnable) }
+        viewModelScope.safeLaunch {
+            val overrideEnable = pluginSettingsRepository.overrideFeatureToggleEnable
+            val allToggles = featureToggleRepository.getAllFeatureToggles()
+            val toggleItems = allToggles.map {
+                FeatureToggleItem(it, ::updateFeatureToggleValue, overrideEnable)
             }
-            .subscribe(
-                { screenState.value = FeatureTogglesState(overrideEnable, it) },
-                { Timber.e(it) }
-            )
-            .autoDispose()
+            screenState.value = FeatureTogglesState(overrideEnable, toggleItems)
+        }
     }
 
     fun resetAll() {
-        featureToggleRepository.resetAll()
-            .subscribe(
-                { loadFeatureToggles() },
-                { Timber.e(it) }
-            )
-            .autoDispose()
+        viewModelScope.safeLaunch {
+            featureToggleRepository.resetAll()
+            loadFeatureToggles()
+        }
     }
 
     fun updateOverrideEnable(newOverrideEnable: Boolean) {
-        featureToggleRepository.updateOverrideEnable(newOverrideEnable)
-            .subscribe(
-                { loadFeatureToggles() },
-                { Timber.e(it) })
-            .autoDispose()
+        viewModelScope.safeLaunch {
+            featureToggleRepository.updateOverrideEnable(newOverrideEnable)
+            loadFeatureToggles()
+        }
     }
 
     private fun updateFeatureToggleValue(featureToggle: FeatureToggle, newValue: Boolean) {
-        featureToggleRepository.updateFeatureToggle(featureToggle.copy(value = newValue))
-            .subscribe(
-                {/*do nothing*/},
-                { Timber.e(it) }
-            )
-            .autoDispose()
+        viewModelScope.safeLaunch {
+            featureToggleRepository.updateFeatureToggle(featureToggle.copy(value = newValue))
+        }
     }
 }
