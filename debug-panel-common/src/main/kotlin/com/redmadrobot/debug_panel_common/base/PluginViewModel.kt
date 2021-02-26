0794f7fff74ce4dfec886a4b6442ec030d30f577
Roman Choryev <r.choryev@redmadrobot.com>
2021-02-26 12:51:43 +0300

    [Base]Удалены Rx зависимости в PluginViewModel

--- debug-panel-common/src/main/kotlin/com/redmadrobot/debug_panel_common/base/PluginViewModel.kt
+++ debug-panel-common/src/main/kotlin/com/redmadrobot/debug_panel_common/base/PluginViewModel.kt
@@ -1,20 +1,5 @@
 package com.redmadrobot.debug_panel_common.base
 
 import androidx.lifecycle.ViewModel
-import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.disposables.Disposable
 
-public open class PluginViewModel : ViewModel() {
-
-    private val compositeDisposable by lazy { CompositeDisposable() }
-
-    override fun onCleared() {
-        compositeDisposable.clear()
-        super.onCleared()
-    }
-
-    protected fun Disposable.autoDispose(): Disposable {
-        compositeDisposable.add(this)
-        return this
-    }
-}
+public open class PluginViewModel : ViewModel()
