3f43a6f7297fe3906efc70b100b134a403d285cb
Roman Choryev <r.choryev@redmadrobot.com>
2022-04-11 22:26:04 +0300

    Sample:Add second debug server

--- sample/src/debug/kotlin/com/redmadrobot/debug_sample/debug_data/DebugServersProvider.kt
+++ sample/src/debug/kotlin/com/redmadrobot/debug_sample/debug_data/DebugServersProvider.kt
@@ -7,7 +7,8 @@ class DebugServersProvider : DebugDataProvider<List<DebugServer>> {
 
     override fun provideData(): List<DebugServer> {
         return listOf(
-            DebugServer(name = "debug 1", url = "https://testserver1.com", isDefault = true)
+            DebugServer(name = "debug 1", url = "https://testserver1.com", isDefault = true),
+            DebugServer(name = "debug 2", url = "https://testserver2.com")
         )
     }
 }
