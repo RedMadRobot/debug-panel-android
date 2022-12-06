cf3f325b8ff8393280cdfee5eba719db96d53dc2
Roman Choryev <r.choryev@redmadrobot.com>
2022-12-06 12:02:25 +0300

    AppSettings:Rename 'app-settings-plugin' package to 'com.redmadrobot.debug.appsettings'

--- plugins/app-settings/src/main/kotlin/com/redmadrobot/debug/appsettings/plugin/AppSettingsPlugin.kt
+++ plugins/app-settings/src/main/kotlin/com/redmadrobot/debug/appsettings/plugin/AppSettingsPlugin.kt
@@ -1,9 +1,9 @@
-package com.redmadrobot.app_settings_plugin.plugin
+package com.redmadrobot.debug.appsettings.plugin
 
 import android.content.SharedPreferences
 import androidx.fragment.app.Fragment
-import com.redmadrobot.app_settings_plugin.data.DefaultSharedPreferences
-import com.redmadrobot.app_settings_plugin.ui.ApplicationSettingsFragment
+import com.redmadrobot.debug.appsettings.data.DefaultSharedPreferences
+import com.redmadrobot.debug.appsettings.ui.ApplicationSettingsFragment
 import com.redmadrobot.debug.core.CommonContainer
 import com.redmadrobot.debug.core.plugin.Plugin
 import com.redmadrobot.debug.core.plugin.PluginDependencyContainer
