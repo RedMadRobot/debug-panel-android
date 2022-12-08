fc59777ecfc62453d825334a6481d45231d2ee66
Roman Choryev <r.choryev@redmadrobot.com>
2022-12-08 18:46:44 +0300

    Flipper:Add the ability to open `flipper` plugin from JetpackCompose

--- plugins/flipper/src/main/kotlin/com/redmadrobot/debug/flipper/plugin/FlipperPlugin.kt
+++ plugins/flipper/src/main/kotlin/com/redmadrobot/debug/flipper/plugin/FlipperPlugin.kt
@@ -1,12 +1,15 @@
 package com.redmadrobot.debug.flipper.plugin
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.viewinterop.AndroidViewBinding
 import androidx.fragment.app.Fragment
 import com.redmadrobot.debug.core.CommonContainer
 import com.redmadrobot.debug.core.extension.getPlugin
 import com.redmadrobot.debug.core.plugin.Plugin
 import com.redmadrobot.debug.core.plugin.PluginDependencyContainer
-import com.redmadrobot.flipper.config.FlipperValue
 import com.redmadrobot.debug.flipper.ui.FlipperFeaturesFragment
+import com.redmadrobot.flipper.config.FlipperValue
+import com.redmadrobot.flipper_plugin.databinding.FragmentContainerFlipperPluginBinding
 import kotlinx.coroutines.flow.Flow
 import java.util.*
 
@@ -53,8 +56,17 @@ public class FlipperPlugin(
         )
     }
 
+    @Deprecated(
+        "You should't use fragments for you plugins. Please use Jetpack Compose",
+        replaceWith = ReplaceWith("content()", "com.redmadrobot.debug.core.plugin.Plugin")
+    )
     override fun getFragment(): Fragment {
         return FlipperFeaturesFragment()
+    }
+
+    @Composable
+    override fun content() {
+        AndroidViewBinding(FragmentContainerFlipperPluginBinding::inflate)
     }
 }
 
