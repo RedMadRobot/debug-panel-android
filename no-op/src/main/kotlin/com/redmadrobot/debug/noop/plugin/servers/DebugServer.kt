e810f08fa716e6758ef5bb646e615a96aaa1a7fd
Roman Choryev <r.choryev@redmadrobot.com>
2023-01-12 15:56:22 +0300

    No-op: update no-op module

--- no-op/src/main/kotlin/com/redmadrobot/debug/noop/plugin/servers/DebugServer.kt
+++ no-op/src/main/kotlin/com/redmadrobot/debug/noop/plugin/servers/DebugServer.kt
@@ -3,5 +3,6 @@ package com.redmadrobot.debug.plugin.servers.data.model
 data class DebugServer(
     val id: Int = 0,
     val name: String,
-    val url: String
+    val url: String,
+    val isDefault: Boolean = false
 )
