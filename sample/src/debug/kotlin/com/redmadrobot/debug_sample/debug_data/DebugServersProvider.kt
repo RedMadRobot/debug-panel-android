7d9f57c6e16c97e58d8a576f38c5270af8789fce
Roman Choryev <r.choryev@redmadrobot.com>
2023-01-16 19:53:14 +0300

    Sample: Update sample project

--- sample/src/debug/kotlin/com/redmadrobot/debug_sample/debug_data/DebugServersProvider.kt
+++ sample/src/debug/kotlin/com/redmadrobot/debug_sample/debug_data/DebugServersProvider.kt
@@ -1,14 +1,44 @@
 package com.redmadrobot.debug_sample.debug_data
 
-import com.redmadrobot.debug.core.data.DebugDataProvider
 import com.redmadrobot.debug.servers.data.model.DebugServer
+import com.redmadrobot.debug.servers.data.model.DebugServerData
+import com.redmadrobot.debug.servers.data.model.DebugStage
 
-class DebugServersProvider : DebugDataProvider<List<DebugServer>> {
+class DebugServersProvider {
 
-    override fun provideData(): List<DebugServer> {
+    fun provideData(): List<DebugServerData> {
         return listOf(
-            DebugServer(name = "debug 1", url = "https://testserver1.com", isDefault = true),
-            DebugServer(name = "debug 2", url = "https://testserver2.com")
+            DebugServer(
+                name = "debug 1",
+                url = "https://testserver1.com",
+                isDefault = true
+            ),
+            DebugServer(
+                name = "debug 2", url = "https://testserver2.com"
+            ),
+            DebugServer(
+                name = "debug 3", url = "https://testserver3.com"
+            ),
+            DebugServer(
+                name = "debug 4", url = "https://testserver4.com"
+            ),
+            DebugStage(
+                name = "debug stage 1",
+                hosts = mapOf(
+                    "main" to "https://testserver1main.com",
+                    "s3" to "https://testserver1s3.com",
+                    "wss" to "https://testserver1wss.com"
+                ),
+                isDefault = true
+            ),
+            DebugStage(
+                name = "debug stage 2",
+                hosts = mapOf(
+                    "main" to "https://testserver2main.com",
+                    "s3" to "https://testserver2s3.com",
+                    "wss" to "https://testserver2wss.com"
+                )
+            ),
         )
     }
 }
--- sample/src/main/kotlin/com/redmadrobot/debug_sample/network/ApiFactory.kt
+++ sample/src/main/kotlin/com/redmadrobot/debug_sample/network/ApiFactory.kt
@@ -1,10 +1,15 @@
 package com.redmadrobot.debug_sample.network
 
-import com.redmadrobot.debug.servers.util.DebugServerInterceptor
+import com.redmadrobot.debug.servers.interceptor.DebugServerInterceptor
+import com.redmadrobot.debug.servers.interceptor.DebugStageInterceptor
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.Response
 import retrofit2.Retrofit
+import java.net.HttpURLConnection
+import java.net.URL
+import java.net.URLConnection
+import java.net.URLStreamHandler
 
 object ApiFactory {
 
@@ -18,9 +23,10 @@ object ApiFactory {
     }
 
     private fun getSampleClient(onCalled: (String) -> Unit): OkHttpClient {
+
         return OkHttpClient.Builder()
             .addInterceptor(
-                DebugServerInterceptor().modifyRequest { request, server ->
+                DebugStageInterceptor("s3").modifyRequest { request, server ->
                     if (server.name == "Test") {
                         request.newBuilder()
                             .addHeader("Authorization", "testToken")
