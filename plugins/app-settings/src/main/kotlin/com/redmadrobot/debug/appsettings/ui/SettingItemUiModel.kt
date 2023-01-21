2d9b5ea8ae157178d92f2c4ebdb2fa4566faa450
Roman Choryev <r.choryev@redmadrobot.com>
2023-01-21 20:41:04 +0300

    AppSettings: Migrate App-settings plugin to the Jetpack Compose

---
+++ plugins/app-settings/src/main/kotlin/com/redmadrobot/debug/appsettings/ui/SettingItemUiModel.kt
@@ -0,0 +1,22 @@
+package com.redmadrobot.debug.appsettings.ui
+
+internal sealed class SettingItemUiModel {
+
+    data class Header(val header: String) : SettingItemUiModel()
+    data class ValueItem(
+        val key: String,
+        var value: Any?,
+    ) : SettingItemUiModel() {
+        fun castToNeededType(newValue: String): Any {
+            return when (value) {
+                is Long -> newValue.toLong()
+                is String -> newValue
+                is Float -> newValue.toFloat()
+                is Int -> newValue.toInt()
+                else -> error("Unexpected type")
+            }
+        }
+    }
+
+    data class BooleanItem(val key: String, var value: Boolean) : SettingItemUiModel()
+}
