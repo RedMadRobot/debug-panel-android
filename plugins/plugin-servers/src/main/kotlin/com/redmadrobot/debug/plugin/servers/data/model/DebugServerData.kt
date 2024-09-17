70ef78d389bbf6d89e3cf7e457517d25d93eae39
a.emogurov <a.emogurov@redmadrobot.com>
2024-09-17 13:18:42 +0400

    servers plugin: remove debug panel internal annotation

--- plugins/plugin-servers/src/main/kotlin/com/redmadrobot/debug/plugin/servers/data/model/DebugServerData.kt
+++ plugins/plugin-servers/src/main/kotlin/com/redmadrobot/debug/plugin/servers/data/model/DebugServerData.kt
@@ -1,8 +1,5 @@
 package com.redmadrobot.debug.plugin.servers.data.model
 
-import com.redmadrobot.debug.core.annotation.DebugPanelInternal
-
-@DebugPanelInternal
 public interface DebugServerData {
     public val id: Int
     public val name: String
