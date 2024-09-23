b9067bf3cd621b49d38dd8e208f1d401fca8f140
Yuriy Elistratov <i.elistratov@redmadrobot.com>
2024-09-23 17:20:14 +0400

    Core: Fix lint error

--- panel-core/build.gradle.kts
+++ panel-core/build.gradle.kts
@@ -38,6 +38,10 @@ android {
         freeCompilerArgs += listOf("-opt-in=kotlin.RequiresOptIn")
     }
 
+    lint {
+        disable += "CoroutineCreationDuringComposition"
+    }
+
     namespace = "com.redmadrobot.debug.core"
 }
 
