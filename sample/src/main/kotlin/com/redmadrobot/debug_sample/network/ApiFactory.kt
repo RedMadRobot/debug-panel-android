2d1b42dee131d77d1eee13865065ea09ccc78494
a.emogurov <a.emogurov@redmadrobot.com>
2026-01-19 17:19:33 +0400

    sample: replace stage with server interceptor

--- sample/src/main/kotlin/com/redmadrobot/debug_sample/network/ApiFactory.kt
+++ sample/src/main/kotlin/com/redmadrobot/debug_sample/network/ApiFactory.kt
@@ -1,17 +1,11 @@
 package com.redmadrobot.debug_sample.network
 
-import com.redmadrobot.debug.plugin.servers.interceptor.DebugStageInterceptor
+import com.redmadrobot.debug.plugin.servers.interceptor.DebugServerInterceptor
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
-import okhttp3.Response
 import retrofit2.Retrofit
-import java.net.HttpURLConnection
-import java.net.URL
-import java.net.URLConnection
-import java.net.URLStreamHandler
 
 object ApiFactory {
-
     fun getSampleApi(onCalled: (String) -> Unit): SampleApi {
         val retrofit = Retrofit.Builder()
             .baseUrl("https://jsonplaceholder.typicode.com/")
@@ -22,25 +16,14 @@ object ApiFactory {
     }
 
     private fun getSampleClient(onCalled: (String) -> Unit): OkHttpClient {
-
         return OkHttpClient.Builder()
+            .addInterceptor(interceptor = DebugServerInterceptor())
             .addInterceptor(
-                DebugStageInterceptor("s3").modifyRequest { request, server ->
-                    if (server.name == "Test") {
-                        request.newBuilder()
-                            .addHeader("Authorization", "testToken")
-                            .build()
-                    } else {
-                        request
-                    }
+                interceptor = Interceptor { chain ->
+                    onCalled.invoke(chain.request().url.toString())
+                    chain.proceed(chain.request())
                 }
             )
-            .addInterceptor(object : Interceptor {
-                override fun intercept(chain: Interceptor.Chain): Response {
-                    onCalled.invoke(chain.request().url.toString())
-                    return chain.proceed(chain.request())
-                }
-            })
             .build()
     }
 }
