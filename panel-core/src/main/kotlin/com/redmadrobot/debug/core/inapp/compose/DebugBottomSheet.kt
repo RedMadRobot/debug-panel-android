6cb3edc510550e8ddeed80bd7b3e6c5987d1d6c3
a.emogurov <a.emogurov@redmadrobot.com>
2026-02-18 14:43:30 +0400

    feature: minor bottom sheet improves

--- panel-core/src/main/kotlin/com/redmadrobot/debug/core/inapp/compose/DebugBottomSheet.kt
+++ panel-core/src/main/kotlin/com/redmadrobot/debug/core/inapp/compose/DebugBottomSheet.kt
@@ -3,17 +3,16 @@
 package com.redmadrobot.debug.core.inapp.compose
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.ModalBottomSheetLayout
 import androidx.compose.material.ModalBottomSheetValue
@@ -28,7 +27,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.redmadrobot.debug.core.extension.getAllPlugins
@@ -38,7 +36,7 @@ import kotlinx.coroutines.launch
 @Composable
 public fun DebugBottomSheet(onClose: () -> Unit) {
     val state = rememberModalBottomSheetState(
-        initialValue = ModalBottomSheetValue.HalfExpanded,
+        initialValue = ModalBottomSheetValue.Expanded,
         confirmValueChange = { sheetState ->
             if (sheetState == ModalBottomSheetValue.Hidden) onClose()
             true
@@ -47,9 +45,11 @@ public fun DebugBottomSheet(onClose: () -> Unit) {
     )
 
     ModalBottomSheetLayout(
+        modifier = Modifier.statusBarsPadding(),
         sheetContent = { BottomSheetContent() },
         sheetState = state,
         scrimColor = Color.Transparent,
+        sheetShape = RoundedCornerShape(size = 16.dp),
         content = {},
     )
 }
@@ -59,35 +59,34 @@ private fun BottomSheetContent() {
     val plugins = remember { getAllPlugins() }
     val pluginsName = remember { plugins.map { it.getName() } }
     val pagerState = rememberPagerState(initialPage = 0, pageCount = { plugins.size })
+
     Column(modifier = Modifier.fillMaxSize()) {
-        Spacer(modifier = Modifier.height(8.dp))
-
-        Box(
+        Divider(
             modifier = Modifier
-                .height(4.dp)
-                .width(50.dp)
-                .clip(CircleShape)
-                .background(Color.Gray)
-                .align(Alignment.CenterHorizontally)
+                .width(width = 50.dp)
+                .padding(vertical = 8.dp)
+                .align(alignment = Alignment.CenterHorizontally),
+            color = Color.Gray,
+            thickness = 4.dp
         )
-
-        Spacer(modifier = Modifier.height(8.dp))
-
-        PluginsTabLayout(pluginsName, pagerState = pagerState)
-
-        PluginsPager(plugins, pagerState = pagerState)
+        PluginsTabLayout(pluginsName = pluginsName, pagerState = pagerState)
+        PluginsPager(plugins = plugins, pagerState = pagerState)
     }
 }
 
 @Composable
 private fun PluginsTabLayout(pluginsName: List<String>, pagerState: PagerState) {
     val scope = rememberCoroutineScope()
+
     ScrollableTabRow(
         selectedTabIndex = pagerState.currentPage,
         backgroundColor = MaterialTheme.colors.background,
+        edgePadding = 16.dp,
         indicator = { tabPositions ->
             TabRowDefaults.Indicator(
-                modifier = Modifier.tabIndicatorOffset(tabPositions[pagerState.currentPage]),
+                modifier = Modifier.tabIndicatorOffset(
+                    currentTabPosition = tabPositions[pagerState.currentPage]
+                ),
                 height = 2.dp,
                 color = MaterialTheme.colors.secondary
             )
@@ -95,7 +94,7 @@ private fun PluginsTabLayout(pluginsName: List<String>, pagerState: PagerState) 
     ) {
         pluginsName.forEachIndexed { index, _ ->
             Tab(
-                text = { Text(pluginsName[index]) },
+                text = { Text(text = pluginsName[index]) },
                 selected = pagerState.currentPage == index,
                 onClick = {
                     scope.launch { pagerState.animateScrollToPage(index) }
