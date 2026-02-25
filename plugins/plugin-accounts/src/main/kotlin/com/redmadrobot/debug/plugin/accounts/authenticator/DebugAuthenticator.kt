9d02d1f55db048f3405c4670798f01fa25a4010b
a.emogurov <a.emogurov@redmadrobot.com>
2026-02-25 15:16:26 +0400

    feature: remove plugin-accounts

--- plugins/plugin-accounts/src/main/kotlin/com/redmadrobot/debug/plugin/accounts/authenticator/DebugAuthenticator.kt
+++
@@ -1,7 +0,0 @@
-package com.redmadrobot.debug.plugin.accounts.authenticator
-
-import com.redmadrobot.debug.plugin.accounts.data.model.DebugAccount
-
-public interface DebugAuthenticator {
-    public fun onAccountSelected(account: DebugAccount)
-}
