e3d710b527d200108b9c4891604837f88c6112b0
a.emogurov <a.emogurov@redmadrobot.com>
2026-03-02 13:35:39 +0400

    feature: remove plugin-appsettings

--- panel-no-op/src/main/kotlin/com/redmadrobot/debug/noop/plugin/appsettings/AppSettingsPlugin.kt
+++
@@ -1,6 +0,0 @@
-package com.redmadrobot.debug.plugin.appsettings
-
-import android.content.SharedPreferences
-import java.util.Collections.emptyList
-
-class AppSettingsPlugin(private val sharedPreferences: List<SharedPreferences> = emptyList())
