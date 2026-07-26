a17d9b42507f36d52367a8ee996dc7b6b24d5510
Yuriy Elistratov <i.elistratov@redmadrobot.com>
2026-07-27 01:04:13 +0400

    build: Update version of version catalog and add konfeature-ui

--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -30,7 +30,7 @@ dependencyResolutionManagement {
     }
 
     versionCatalogs {
-        val version = "2026.02.23" // Keep it in sync with root settings.gradle.kts
+        val version = "2026.07.10" // Keep it in sync with root settings.gradle.kts
         create("rmr") {
             from("com.redmadrobot.versions:versions-redmadrobot:$version")
         }
@@ -39,9 +39,6 @@ dependencyResolutionManagement {
         }
         create("stack") {
             from("com.redmadrobot.versions:versions-stack:$version")
-        }
-        create("libs") {
-            from(files("../gradle/libs.versions.toml"))
         }
     }
 }
